e5bebb0abe9d591a274ff1d14889b97d0e38e00e
chasefarmer2808 <chasefarmer2808@ufl.edu>
2020-04-27 21:26:08 -0400

    import counties script

---
+++ db/import_counties_csv.sh
@@ -0,0 +1,12 @@
+read_var() {
+    VAR=$(grep $1 $2 | xargs)
+    IFS="=" read -ra VAR <<< "$VAR"
+    echo ${VAR[1]}
+}
+
+DB_USER=$(read_var DB_USER ../.env)
+DB_PASSWORD=$(read_var DB_PASSWORD ../.env)
+
+wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
+mongoimport --uri "mongodb+srv://${DB_USER}:${DB_PASSWORD}@covidhero-b5qz6.mongodb.net/covidhero?retryWrites=true&w=majority" -c counties --type csv --headerline --file us-counties.csv
+rm us-counties.csv
